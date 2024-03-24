15d81bfa4684c348daf0eb8811c01290a0fc0264
garuspick24 <122586302+garuspick24@users.noreply.github.com>
2024-03-24 21:01:36 +0200

    1

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 url = 'https://www.psychologytoday.com/us/therapists/ny/brooklyn'
+responce =  requests.get(url)
+html = responce.text
 
+soup = BeautifulSoup(html, 'html.parser')
